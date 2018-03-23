e30b70e0b7d96868521e223af4edf47b0a4b1672
Guest User <Guest@Epicodus-3E.local>
2018-03-23 13:22:01 -0700

    remove ; from .env that was giving 401 error, change api call parameters, change main.js

--- src/DoctorLookup.js
+++ src/DoctorLookup.js
@@ -1,31 +1,30 @@
-import $ from 'jquery';
+import $ from 'jquery'
 
 export class DoctorLookup {
   constructor(search){
-    this.apiKey = process.env.exports.apiKey;
-    this.search = "Back%20Injury";
+    this.apiKey = process.env.exports.apiKey
+    this.search = search
   }
 
   betterDoctorSearch(doctorDisplay){
-    let searchQuery = this.search;
-    console.log(searchQuery);
-    let key = process.env.exports.apiKey;
-    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=back%20injury&location=or-portland&skip=0&limit=10&user_key=' + key;
-    console.log(url);
+    let searchQuery = this.search
+    console.log(searchQuery)
+    let key = process.env.exports.apiKey
+    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=' + searchQuery + '&location=or-portland&sort=best-match-desc&skip=0&limit=100&user_key=' + key
+    console.log(url)
     $.ajax({
-      headers: (key),
       url: (url),
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response){
-        let doctorInfo = [];
-        for (let i = 0; i < 10; i++){
+        let doctorInfo = []
+        for (let i = 0; i < 100; i++){
           doctorInfo.push(response.data[i])
         }
-        console.log(doctorInfo);
-        doctorDisplay(doctorInfo);
+        console.log(doctorInfo)
+        doctorDisplay(doctorInfo)
       },
       error: function(){
 
--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-import './styles.css';
-import $ from 'jquery';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { DoctorLookup } from './DoctorLookup.js';
+import './styles.css'
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { DoctorLookup } from './DoctorLookup.js'
 
 
 $(document).ready(function() {
-  let newDoctorLookup = new DoctorLookup("placeholder");
-  let doctorDisplay = function displayResults(array){
 
-  }
-  newDoctorLookup.betterDoctorSearch(doctorDisplay);
   $('#doctorSearchFormButton').click(function(){
-    let search = $('#doctorSearchInput').val();
+    let search = $('#doctorSearchInput').val()
+    console.log(search)
+    let newDoctorLookup = new DoctorLookup(search)
+    let doctorDisplay = function displayResults(array){
+
+    }
+    newDoctorLookup.betterDoctorSearch(doctorDisplay)
   })
-});
+})
