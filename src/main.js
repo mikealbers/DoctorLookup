dbc16d1fa0c9f34152d2f5c2372a0fd9466daab5
Guest User <Guest@Epicodus-3E.local>
2018-03-23 14:08:44 -0700

    main.js: add doctor object, add loop for displaying doctors

--- src/main.js
+++ src/main.js
@@ -11,7 +11,30 @@ $(document).ready(function() {
     console.log(search)
     let newDoctorLookup = new DoctorLookup(search)
     let doctorDisplay = function displayResults(array){
+      for(let i = 0; i < 10; i++){
+        let doctor = {
+          Id: array[i].uid,
+          Name: array[i].profile.first_name + ' ' + array[i].profile.last_name,
+          Bio: array[i].profile.bio,
+          Picture: array[i].profile.image_url,
+          Practice: array[i].practices[0].name,
+          Address: {
+            city: array[i].practices[0].visit_address.city,
+            state: array[i].practices[0].visit_address.state_long,
+            street: array[i].practices[0].visit_address.street,
+            zip: array[i].practices[0].visit_address.zip
+          },
+        }
 
+        $('#doctorResults').append(
+          '<div id=' + doctor.Id + '>' +
+          '<h3>' + doctor.Name + '</h3>' +
+          '<img src="'+ doctor.Picture +'" alt="">' +
+          '<p>' + doctor.Bio + '</p>' +
+          '</div>'
+
+        )
+      }
     }
     newDoctorLookup.betterDoctorSearch(doctorDisplay)
   })
