89c6f5fe8553aa9797fef70dbf12421527cf1e79
Guest User <Guest@Epicodus-3E.local>
2018-03-23 12:20:52 -0700

    move key to .env, update html, restructure betterDoctorSearch()

--- src/DoctorLookup.js
+++ src/DoctorLookup.js
@@ -1,21 +1,31 @@
 import $ from 'jquery';
 
 export class DoctorLookup {
-  constructor(){
-    this.apiKey = exports.apiKey;
+  constructor(search){
+    this.apiKey = process.env.exports.apiKey;
+    this.search = "Back%20Injury";
   }
 
-  doctorSearch(){
+  betterDoctorSearch(doctorDisplay){
     let searchQuery = this.search;
-    const url = ("https://api.betterdoctor.com/2016-03-01/doctors?query=" + this.search + "&location=or-portland&skip=0&limit=10&user_key=" + this.apiKey)
+    console.log(searchQuery);
+    let key = process.env.exports.apiKey;
+    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=back%20injury&location=or-portland&skip=0&limit=10&user_key=' + key;
+    console.log(url);
     $.ajax({
+      headers: (key),
       url: (url),
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response){
-
+        let doctorInfo = [];
+        for (let i = 0; i < 10; i++){
+          doctorInfo.push(response.data[i])
+        }
+        console.log(doctorInfo);
+        doctorDisplay(doctorInfo);
       },
       error: function(){
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DoctorLookup } from './DoctorLookup.js';
 
+
 $(document).ready(function() {
-  
+  let newDoctorLookup = new DoctorLookup("placeholder");
+  let doctorDisplay = function displayResults(array){
+
+  }
+  newDoctorLookup.betterDoctorSearch(doctorDisplay);
+  $('#doctorSearchFormButton').click(function(){
+    let search = $('#doctorSearchInput').val();
+  })
 });
